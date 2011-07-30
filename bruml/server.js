3e13dad1d3c81341518f4e251eff027568b30dce
chapel <jacob.chapel@gmail.com>
2011-07-29 22:47:10 -0700

    Re-factor lecture broadcasting, move javascript code block to separate file from index

--- bruml/server.js
+++ bruml/server.js
@@ -8,12 +8,14 @@ app.configure(function() {
   app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
 });
 
+var clients = {};
 
 var posts = {};
 
 app.listen('8080');
 
 io.sockets.on('connection', function(socket) {
+  /*
   socket.on('lectureID', function(id) {
     console.log('lectureID', id)
     socket.set('lectureID', id);
@@ -22,54 +24,70 @@ io.sockets.on('connection', function(socket) {
     }
     socket.json.send({posts: posts[id]});
   })
+  */
+  socket.on('subscribe', function(lecture) {
+    var id = socket.id;
+    clients[id] = {
+      socket: socket,
+      lecture: lecture
+    }
+    if (!posts[lecture]) {
+      posts[lecture] = [];
+    }
+    socket.json.send({posts: posts[lecture]});
+  })
   socket.on('post', function(post, id) {
-    console.log(id)
-    socket.get('lectureID', function(err, id) {
-      if (!posts[id]) posts[id] = [];
-      post.postid = posts[id].length + 1;
-      post.meetingid = id;
-      post.comments = [];
-      console.log("POST " + post.postid + " received from client: " + socket.id);
-      posts[id].push(post);
-      console.log("Now have " + posts[id].length + " posts in all");
-      console.log(id)
-      io.sockets.emit('post', post, id);
-    });
+    if (!posts[id]) posts[id] = [];
+    post.postid = posts[id].length + 1;
+    post.meetingid = id;
+    post.comments = [];
+    posts[id].push(post);
+    publish({post: post}, id);
+    //io.sockets.emit('post', post, id);
   });
 
   socket.on('vote', function(vote, id) {
-    socket.get('lectureID', function(err, id) {
-      _posts = posts[id].map(function(post) {
-        if(post.postid == vote.postid) {
-          switch (vote.direction) {
-            case 'up': 
-              post.posvotes++;
-              break;
-            case 'down':
-              post.negvotes++;
-              break;
-          }
+    _posts = posts[id].map(function(post) {
+      if(post.postid == vote.postid) {
+        switch (vote.direction) {
+          case 'up': 
+            post.posvotes++;
+            break;
+          case 'down':
+            post.negvotes++;
+            break;
         }
-        return post;
-      });
-      posts[id] = _posts;
-      io.sockets.emit('vote', vote, id);
-    })
+      }
+      return post;
+    });
+    posts[id] = _posts;
+    publish({vote: vote}, id);
+    //io.sockets.emit('vote', vote, id);
   })
 
   socket.on('comment', function(comment, id) {
-    socket.get('lectureID', function(err, id) {
-      _posts = posts[id].map(function(post) {
-        if(post.postid == comment.postid) {
-          if (!post.comments) {
-            post.comments = [];
-          }
-          post.comments.push(comment);
+    _posts = posts[id].map(function(post) {
+      if(post.postid == comment.postid) {
+        if (!post.comments) {
+          post.comments = [];
         }
-        return post;
-      });
-      posts[id] = _posts;
-      io.sockets.emit('comment', comment, id);
-    })
+        post.comments.push(comment);
+      }
+      return post;
+    });
+    posts[id] = _posts;
+    publish({comment: comment}, id);
+  })
+  
+  socket.on('disconnect', function() {
+    delete clients[socket.id];
   })
 });
+
+function publish(data, lecture) {
+  Object.getOwnPropertyNames(clients).forEach(function(id) {
+    if (clients[id].lecture === lecture) {
+      clients[id].socket.json.send(data)
+    }
+  })
+}
