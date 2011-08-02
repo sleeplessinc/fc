b231dc2f23c7ec875af0fa340e2a4e2bbac7a153
Joe Hitchens <joe@sleepless.com>
2011-08-01 17:16:58 -0700

    changes to boot and start scripts

--- util/boot.sh
+++ util/boot.sh
@@ -23,7 +23,22 @@ yes | yum --nogpgcheck install mysql-server
 # /etc/init.d/mysqld start
 # /usr/bin/mysqladmin -u root password 'foobarbazquX'
 
+# install mongodb
+mongover="1.8.2"
+if test ! -e mongodb.tgz ; then
+	curl http://fastdl.mongodb.org/linux/mongodb-linux-i686-$mongover.tgz > mongodb-linux-i686-$mongover.tgz
+	tar xzf mongodb-linux-i686-$mongover.tgz
+	cd mongodb-linux-i686-$mongover/bin
+	chmod a+rx *
+	chmod uo-w *
+	cp -f * /usr/local/bin
+	echo "/usr/local/bin &> /var/log/mongod.log &" >> /etc/rc.local
+	mkdir -p /data/db
+	/usr/local/bin/mongod -v &> /var/log/mongod.log &
+fi
 
+
+# install node
 nodever="v0.4.10"
 if test ! -e node-$nodever ; then
         curl http://nodejs.org/dist/node-$nodever.tar.gz > node-$nodever.tar.gz
@@ -34,7 +49,7 @@ if test ! -e node-$nodever ; then
         make install
 fi
 
-
+# install npm
 if test ! -e npm ; then
         git clone http://github.com/isaacs/npm.git
         cd npm
