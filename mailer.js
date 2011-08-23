f748167351a2cfc76b9d4d1ca14eb1bf49a7edf3
Colin Smith <colin@blkcat.net>
2011-08-23 15:51:23 -0400

    Move emailer to its own module and use EJS for email templates.

---
+++ mailer.js
@@ -0,0 +1,57 @@
+/* vim: set ts=2: */
+
+// Core
+var fs	= require( 'fs' );
+
+// Amazon
+var aws	= require( 'aws-lib' );
+var hat	= require( 'hat' );
+
+// Templating
+var ejs	= require( 'ejs' );
+
+function buildPath( template ) {
+	return './emails/' + template + '.ejs';
+}
+
+function Mailer( accessKey, secretKey ) {
+	this.accessKey	= accessKey;
+	this.secretKey	= secretKey;
+
+	this.client			= aws.createSESClient( this.accessKey, this.secretKey );
+}
+	
+module.exports = Mailer;
+
+Mailer.prototype.send = function( msg, callback ) {
+	if( ! msg.template && ! msg.body ) {
+		callback( new Error( 'Invalid message specification!' ) );
+	}
+
+	if( msg.template ) {
+		var templatePath = buildPath( msg.template );
+
+		var data = fs.readFileSync( templatePath, 'utf8' );
+
+		msg.body = ejs.render( data, { locals: msg.locals } ); // .replace( /\\n/g, '\n' );
+	}
+
+	var params = {
+		'Destination.ToAddresses.member.1'	: msg.to,
+		'Message.Body.Test.Charset'					: 'UTF-8',
+		'Message.Body.Text.Data'						: msg.body,
+		'Message.Subject.Charset'						: 'UTF-8',
+		'Message.Subject.Data'							: msg.subject,
+		'Source'														: 'jacob@sleepless.com' // 'FinalsClub.org Mailer <mailer@finalsclub.org>'
+	};
+
+	this.client.call( 'SendEmail', params, function( result ) {
+		console.log( result );
+
+		if( result[ '@' ].Error ) {
+			callback( result[ '@' ].Error );
+		} else {
+			callback( result );
+		}
+	});
+}
