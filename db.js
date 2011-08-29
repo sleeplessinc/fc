0ea83e3ce2e94adc6211aab82ac58066425883f2
Colin Smith <colin@blkcat.net>
2011-08-29 13:45:45 -0400

    Revert "Add replica set support to app.js and db.js. This is still fairly kludgy; in particular the distinction between mongoHost and mongoSet probably doesn't need to be there."
    This reverts commit f64343368d902e62de47ccac8aa206045a0d9d9f.

--- db.js
+++ db.js
@@ -1,94 +1,55 @@
 /* vim: set ts=2: */
 
 var url		= require( 'url' );
-var async	= require( 'async' );
 
 var mongo	= require( 'mongodb' );
 
-var open = module.exports.open = function( spec, db, collection, callback ) {
-	if( ! spec ) {
-		throw new Error( 'No connection specification defined!' );
+module.exports.open = function( uri, callback ) {
+	if( ! uri ) {
+		throw new Error( 'No URI defined!' );
 	}
 
-	if( typeof db === 'function' ) {
-		callback = db;
+	var parsed			= url.parse( uri );
+	var path				= parsed.pathname.split( '/' );
 
-		db = undefined;
+	var port				= parsed.port || 27017;
+	var host				= parsed.hostname || 'localhost';
+
+	var db					= path[ 1 ];
+	var collection	= path[ 2 ];
+
+	if( parsed.auth ) {
+		var auth = parsed.auth.split( ':' );
+
+		var username = auth[ 0 ];
+		var password = auth[ 1 ];
 	}
 
-	if( typeof collection === 'function' ) {
-		callback = collection;
-
-		collection = undefined;
+	if( ! db ) {
+		throw new Error( 'Unable to parse database name!' );
 	}
 
-	var uris				= spec.split( ',' );
-	var servers			= [];
+	var server		= new mongo.Server( host, port, { auto_reconnect : true } );
+	var database	= new mongo.Db( db, server );
 
-	async.forEachSeries(
-		uris,
+	database.open( function( err, database ) {
+		// XXX: add authentication
 
-		function( uri, next ) {
-			var parsed			= url.parse( uri );
-			var path				= parsed.pathname.split( '/' );
-
-			var port				= parsed.port			|| 27017;
-			var host				= parsed.hostname;
-
-			if( path[ 1 ] ) {
-				db = path[ 1 ];
-			}
-
-			if( path[ 2 ] ) {
-				collection = path[ 2 ];
-			}
-
-			if( ! host ) {
-				next( new Error( 'No host defined!' ) );
+		if( err ) {
+			callback( err );
+		} else {
+			// do we want to open a specific collection? otherwise just return DB
+			if( ! collection ) {
+				callback( null, database );
 			} else {
-				servers.push( new mongo.Server( host, port, { auto_reconnect : true } ) );
-
-				next();
-			}
-		},
-
-		function( err ) {
-			if( err ) {
-				throw( err );
-			}
-
-			if( ! db ) {
-				throw new Error( 'Unable to parse database name!' );
-			}
-
-			if( servers.count > 1 ) {
-				var serverSpec = new ReplSetServers( servers );
-			} else {
-				var serverSpec = servers.shift();
-			}
-
-			var database	= new mongo.Db( db, serverSpec );
-
-			database.open( function( err, database ) {
-				// XXX: add authentication
-
-				if( err ) {
-					callback( err );
-				} else {
-					// do we want to open a specific collection? otherwise just return DB
-					if( ! collection ) {
-						callback( null, database );
+				database.createCollection( collection, function( err, c ) {	
+					if( err ) {
+						callback( err );
 					} else {
-						database.createCollection( collection, function( err, c ) {	
-							if( err ) {
-								callback( err );
-							} else {
-								callback( null, c );
-							}
-						});
+						callback( null, c );
 					}
-				}	
-			});
-		}
-	);
+				});
+			}
+		}	
+	});
 }
